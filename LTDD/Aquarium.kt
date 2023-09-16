c6bc1a5a897cb953cc6d1c080d3a80d6aca4ee27
Đinh Quang Huy <107740423+qhuy1006@users.noreply.github.com>
2023-09-16 23:38:33 +0700

    huy
    huy

---
+++ LTDD/Aquarium.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import java.sql.DriverManager.println
+
+
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+    fun printSize(){
+        println("Width:  $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
